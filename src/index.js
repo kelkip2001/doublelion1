cfd964a27ac081bd5440d2309350851efc6b72af
Kelvin Sing' oei <79026959+kelkip2001@users.noreply.github.com>
2021-11-17 11:26:39 +0300

    stage 2

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../src/assests/css/custom.css';
 import '../src/assests/css/fontawesome.css';
+import '../src/assests/css/animate.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
