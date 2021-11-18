89dafe85f71d2a8e7172458958a619a4a40a1388
Kelvin Sing' oei <79026959+kelkip2001@users.noreply.github.com>
2021-11-18 18:18:12 +0300

    front 1

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,14 @@ import React, { Component, Fragment } from 'react';
 import Categories from '../components/Home/Categories';
 import Collection from '../components/Home/Collection';
 import FeatureProducts from '../components/Home/FeatureProducts';
+import NewArrival from '../components/Home/NewArrival';
 
 export class HomePage extends Component {
     render() {
         return (
             <Fragment>
                 <FeatureProducts />
+                <NewArrival />
                 <Categories />
                 <Collection /> 
             </Fragment>
